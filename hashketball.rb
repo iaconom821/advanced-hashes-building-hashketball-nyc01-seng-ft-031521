01125dc4310cd57f89b3eee5d9c5ae2ae26a0d64
Michael Iacono <iaconom821@gmail.com>
2021-02-19 22:40:25 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -38,7 +38,7 @@ def game_hash
         blocks: 1,
         slam_dunks: 15
       }, {
-        player_name: 'Mason Plumless',
+        player_name: 'Mason Plumlee',
         number: 1,
         shoe: 19,
         points: 26,
