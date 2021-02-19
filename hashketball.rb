cbcab70f58cdf9495943cdc8c3360a1792d3ef60
Michael Iacono <iaconom821@gmail.com>
2021-02-19 22:21:27 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,1 +1,16 @@
 # Write your code here!
+require 'yaml'
+require 'pry'
+
+def game_hash
+  the_game = {
+    home: {
+      team_name: = "",
+      colors: = [],
+      players: = [{
+        player_name: =
+      }]
+    },
+    away: {},
+  }
+end
